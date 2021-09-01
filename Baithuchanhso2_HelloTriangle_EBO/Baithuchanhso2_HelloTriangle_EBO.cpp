5a12483acc002b1535673ea6e5dc71e43dd30222
hainguyen210 <60843704+hainguyen210@users.noreply.github.com>
2021-09-01 21:52:07 +0700

    Initialize 63IT3

---
+++ Baithuchanhso2_HelloTriangle_EBO/Baithuchanhso2_HelloTriangle_EBO.cpp
@@ -0,0 +1,180 @@
+﻿// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+
+//function
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+const GLuint WIDTH = 800, HEIGHT = 600;
+//bước 0.0: chuẩn bị shader
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout (location=0) in vec3 position;\n"
+"out vec4 vertexColor;\n"
+"void main()\n"
+"{\n"
+"gl_Position= vec4(position.x,position.y,position.z,1);\n"
+"vertexColor=vec4(position.x,position.y,position.z,1);\n"
+"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"in vec4 vertexColor;\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color= vertexColor;\n"
+"}\0";
+
+int main(void)
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Tạo 1 cửa sổ GLFWwindow 
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Bai thuc hanh so 2 - Hello Triangle", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+
+	//Đặt biến glewExperimental về true  (bắt buộc)
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	// Định nghĩa kích thước viewport
+	 // Define the viewport dimensions
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);    // lấy kích thước framebuffer   (chú ý)
+	glViewport(0, 0, width, height);;
+
+	////// bước 0.1 Build và biên dịch shader program
+	// Vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Check for compile time errors
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// Fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Check for compile time errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// Link shaders
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	//bước 1: khai báo vertex input (vertex data)
+	GLfloat vertices_hinhvuong[] = {
+		0.5f,0.5f,0.0f,  //top-right 0
+		0.5f,-0.5f,0.0f,   //bottom right 1 
+		-0.5f,-0.5f,0.0f,    //bot Left 2
+	    -0.5f,0.5f,0.0f    //Top Left  3
+	};
+	GLuint indices[] = {
+	    0,1,3,	//tam giac 1
+		1,2,3	//tam giac 2
+	};
+
+	//Bước 2: Khởi tạo VBO, VAO,EBO
+		//b2.1 VAO
+	GLuint VAO;
+	GLuint VBO;
+	GLuint EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	//bind VAO
+	glBindVertexArray(VAO);
+		//b2.2 VBO
+		glBindBuffer(GL_ARRAY_BUFFER, VBO); // liên kết (bind) VBO
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices_hinhvuong), vertices_hinhvuong, GL_STATIC_DRAW);
+		//b2.3 EBO
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		//set attribute point
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), (GLvoid*)0);
+		glEnableVertexAttribArray(0);
+
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); không đc unbind EBO
+		glBindBuffer(GL_ARRAY_BUFFER, 0); //unbind VBO , cho phép gọi hàm glVertexAttribPointer trong VBO
+//unbind VAO
+	glBindVertexArray(0);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINES);
+	//Game Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// check sự kiện  (ấn nút bàn phím, chuột,...)
+		glfwPollEvents();
+		//Render
+		//xóa color buffer
+		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//Buoc 3 Vẽ hình 1 hình tam giác
+
+		glUseProgram(shaderProgram);
+
+		glBindVertexArray(VAO);
+		/*glDrawArrays(GL_TRIANGLES, 0, 6);*/
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);  //glDrawElements(Primitive type,count- số đỉnh vẽ,kiểu dữ liệu của indices,0);
+		glBindVertexArray(0);
+
+		///swap
+		glfwSwapBuffers(window);
+	}
+	//Terminate GLFW, xóa và dọn dẹp tài nguyên sau khi thoát
+	glfwTerminate();
+	return 0;
+
+}
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	std::cout << key << std::endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
